c010063ac6c284428e92e3b351d74fbcee6dd89b
payalgoyal <payalgoyal90@gmail.com>
2017-04-09 22:43:20 +0530

    remove prev values on correct ans

--- colorMatch.js
+++ colorMatch.js
@@ -6,6 +6,14 @@ var queryLetter;
 	var boxImages = [];
 
 function colorMatch(){
+	var playArea = document.getElementById("play_area");
+	while (playArea.firstChild){
+		playArea.removeChild(playArea.firstChild);
+	}
+	var foot = document.getElementById("footer");
+	while (foot.firstChild){
+		foot.removeChild(foot.firstChild);
+	}
 	queryImages.push("images/RedStar.png","images/BlueStar.png");
 	
 	var queryArea = document.getElementById("play_area");
@@ -14,8 +22,8 @@ function colorMatch(){
 		correct: 0});
 		
 	list.push({key: "BLUE",
-		optionValue: ["images/RedBox.png","images/BlueBox.png"],
-		correct: 1});
+		optionValue: ["images/BlueBox.png","images/RedBox.png"],
+		correct: 0});
 	
 	optionKeys = [];
 	ran = Math.floor(Math.random() * list.length);
@@ -24,18 +32,14 @@ function colorMatch(){
 	}
 	document.getElementById("header").innerHTML = "Match Color";
 	
-	var playArea = document.getElementById("play_area");
-	
 	for (var box=0;box<list[ran].optionValue.length;box++){
 		var boxImg = document.createElement("img");
 		boxImg.setAttribute("src",list[ran].optionValue[box]);
-		boxImg.setAttribute("style","width:10%;height:80%");
+		boxImg.setAttribute("style","width:40%;height:80%");
 		boxImg.setAttribute("id","queryCont"+box);
 	
 		playArea.appendChild(boxImg);
 	}
-	
-	var foot = document.getElementById("footer");
 	
 	var queryImg = document.createElement("img");
 	queryImg.setAttribute("src",queryImages[ran]);
